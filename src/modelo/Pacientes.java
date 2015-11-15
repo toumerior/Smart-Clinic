d8be317182426d639a54666f90ba4ee182ca50d0
toumerior <toumerior@BRUNO>
2015-11-15 19:07:23 -0200

    Change-Id: I7c888465ea44cb81c12076655cb376886a7e7b53

---
+++ src/modelo/Pacientes.java
@@ -0,0 +1,31 @@
+package modelo;
+
+public class Pacientes {
+    private int paciente_id;
+    private int pessoa_id;
+    private int convenio_id;
+    
+	public int getPaciente_id() {
+		return paciente_id;
+	}
+	public void setPaciente_id(int paciente_id) {
+		this.paciente_id = paciente_id;
+	}
+	
+	public int getPessoa_id() {
+		return pessoa_id;
+	}
+	
+	public void setPessoa_id(int pessoa_id) {
+		this.pessoa_id = pessoa_id;
+	}
+	
+	public int getConvenio_id() {
+		return convenio_id;
+	}
+	
+	public void setConvenio_id(int convenio_id) {
+		this.convenio_id = convenio_id;
+	}
+    
+}
