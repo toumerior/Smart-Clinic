eab5c64e2c861f462c829f1ee8e5036371f973bb
Pedro Henrique de Souza <pedroccmp@hotmail.com>
2015-11-26 23:05:40 -0200

    Problema de conexão resolvido

--- src/conectores/ConnectionFactory.java
+++ src/conectores/ConnectionFactory.java
@@ -4,15 +4,16 @@ package conectores;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import com.mysql.jdbc.Driver;
 
 public class ConnectionFactory {
 	public Connection getConnection() {
-	    
 	    	 //Class.forName("com.mysql.jdbc.Driver");
-	    	 //DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+	    	 
 	         try {
+	        	 DriverManager.registerDriver(new com.mysql.jdbc.Driver());
 				return DriverManager.getConnection(
-				   "jdbc:mysql://localhost/smart_clinic", "root", "brunopr"
+				   "jdbc:mysql://localhost:3306/teste", "root", "3512655"
 				 );
 			} catch (SQLException e) {
 				throw new RuntimeException(e);
